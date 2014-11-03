37e651ff6237cb1779d249b7f481064719bdd5d4
Douglas Busck <douglas.busck@gmail.com>
2014-11-03 16:57:21 +0100

    Updated brand colors, new logo

--- js/application.js
+++ js/application.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-	$('douglas').isAwesome('infinite');
+$(function() {
+    //caches a jQuery object containing the header element
+    var header = $("header");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        var heroHeight = $('.cover-image').outerHeight();
+        var headerHeight = $('header').outerHeight();
+
+        if (scroll >= heroHeight - headerHeight) {
+            header.addClass('fixed');
+        } else {
+            header.removeClass("fixed");
+        }
+    });
 });
